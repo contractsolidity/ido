f3e7af94de0d4e7f8e068e549a4caa7fdf6d1a67
contractsolidity <113483274+contractsolidity@users.noreply.github.com>
2025-12-08 21:08:28 +0800

    Add files via upload

---
+++ assets/transactions-DawcnscM.js
@@ -0,0 +1,16 @@
+import{h as f,i as a,x as m}from"./index-B6MMt_rg.js";import{c as p}from"./index-H_CLKlrd.js";import"./index-CSbTA8WD.js";import"./index-CNw6pGYw.js";import"./if-defined-BvoOKPDa.js";import"./index-CDHFDgCW.js";import"./index-BNpjolJp.js";import"./index-B8_sBK1t.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
